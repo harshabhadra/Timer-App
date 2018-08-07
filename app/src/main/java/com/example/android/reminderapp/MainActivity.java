e43ac97284229d461a227f29c698141e322b67a2
harshabhadra <harshasharkey@gmail.com>
2018-08-07 20:09:49 +0530

    Add appp and gradl file

---
+++ app/src/main/java/com/example/android/reminderapp/MainActivity.java
@@ -0,0 +1,172 @@
+package com.example.android.reminderapp;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int NO_IMAGE = -1;
+    long entry;
+    boolean stop = false;
+    boolean isStop = false;
+    CountDownTimer countDownTimer;
+    MediaPlayer mediaPlayer;
+    AudioManager audioManager;
+
+    AudioManager.OnAudioFocusChangeListener changeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                mediaPlayer.pause();
+                mediaPlayer.seekTo(1);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                mediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                releaseMediaPlayer();
+            }
+        }
+    };
+    private MediaPlayer.OnCompletionListener completionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
+    public long getEntry(long mHr, long mMin, long mSec) {
+
+        long mEntry = (mHr + mMin + mSec);
+
+        return mEntry;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        final TextView timeLeft = findViewById(R.id.timeLeft);
+        final TextView result = findViewById(R.id.result);
+        result.setVisibility(View.INVISIBLE);
+        final EditText entrySec = findViewById(R.id.entrySec);
+        final EditText entryHour = findViewById(R.id.entryHour);
+        final EditText entryMinute = findViewById(R.id.entryMin);
+        final LinearLayout timer = findViewById(R.id.timer);
+
+        Button start = findViewById(R.id.button);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                timer.setVisibility(View.VISIBLE);
+                result.setVisibility(View.GONE);
+                stop = false;
+                isStop = false;
+                long hr = Long.parseLong(entryHour.getText().toString()) * 3600;
+                final long min = Long.parseLong(entryMinute.getText().toString()) * 60;
+                long sec = Long.parseLong(entrySec.getText().toString());
+
+                entry = getEntry(hr, min, sec);
+
+                countDownTimer = new CountDownTimer(entry * 1000, 100) {
+
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        if (isStop) {
+                            millisUntilFinished = 0;
+                            timeLeft.setText(String.valueOf(millisUntilFinished));
+                            String text = String.format(Locale.getDefault(), "%02d hr %02d min %02d sec",
+                                    TimeUnit.MILLISECONDS.toHours(millisUntilFinished) % 60,
+                                    TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) % 60,
+                                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60);
+                            timer.setVisibility(View.INVISIBLE);
+                            timeLeft.setText(text);
+                            timeLeft.setVisibility(View.VISIBLE);
+                            cancel();
+
+                        } else {
+                            timeLeft.setText(String.valueOf(millisUntilFinished / 1000));
+                            String text = String.format(Locale.getDefault(), "%02d hr %02d min %02d sec",
+                                    TimeUnit.MILLISECONDS.toHours(millisUntilFinished) % 60,
+                                    TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) % 60,
+                                    TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60);
+                            timeLeft.setText(text);
+                            timeLeft.setVisibility(View.VISIBLE);
+                            timer.setVisibility(View.GONE);
+                        }
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        result.setVisibility(View.VISIBLE);
+                        result.animate().rotationX(360).setDuration(1000);
+                        countDownTimer.cancel();
+
+                        int result = audioManager.requestAudioFocus(changeListener, AudioManager.STREAM_MUSIC
+                                , AudioManager.AUDIOFOCUS_GAIN);
+
+                        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+
+                            mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.ring);
+                            mediaPlayer.start();
+
+                            mediaPlayer.setOnCompletionListener(completionListener);
+                        }
+
+
+                    }
+                }.start();
+            }
+        });
+
+        Button stopButton = findViewById(R.id.button2);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isStop = true;
+            }
+        });
+
+        result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                releaseMediaPlayer();
+                timeLeft.setVisibility(View.GONE);
+                timer.setVisibility(View.VISIBLE);
+                result.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mediaPlayer != null) {
+
+            mediaPlayer.release();
+
+            mediaPlayer = null;
+            audioManager.abandonAudioFocus(changeListener);
+        }
+    }
+}
